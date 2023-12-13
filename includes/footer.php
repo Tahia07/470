273caa21aadc31ba27e9ca8929eae4993089d167
Tahia <tahiatun.nazi@g.bracu.ac.bd>
2023-12-14 00:57:29 +0600

    pushing

---
+++ includes/footer.php
@@ -0,0 +1,68 @@
+<br>
+<strong>
+    <div class="container-fluid text-center  text-white" style="background-color: #000000;">
+        <br>
+        <div class=" row">
+            <div class="col-sm-4">
+                <span class="glyphicon glyphicon-off"></span>
+                <h4>Quality Ensured</h4>
+                <br>
+                <p>Our Agents Checks Every Minute for Updates 24/7</p>
+            </div>
+            <div class="col-sm-4">
+                <span class="glyphicon glyphicon-heart"></span>
+                <h4>Verified Sellers</h4>
+                <br>
+                <p>You Can Trust Blindly, Our Page is Certified And Monitized By Government</p>
+            </div>
+            <div class="col-sm-4">
+                <span class="glyphicon glyphicon-lock"></span>
+                <h4>Social Media</h4>
+                <br>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="" style="color: white; margin-right :20px;"> <i class="fab fa-facebook fa-3x">&nbsp;</i></a>
+                <a href="" style="color: white; margin-right :20px;"><i class="fab fa-instagram fa-3x">&nbsp;</i></a>
+                <a href="" style="color: white; margin-right :20px;"><i class="fab fa-twitter fa-3x"></i></a>
+            </div>
+        </div>
+        <br><br>
+        <div class="row">
+            <div class="col-sm-4">
+                <span class="glyphicon glyphicon-leaf"></span>
+                <h4>ASK QUESTION</h4>
+                <br>
+                <a href="https://helpcenter.daraz.com.bd/page/knowledge?pageId=11&category=1000001032&knowledge=1000005321&language=en" style="color: red; text-decoration: none;">
+                    <h5>FAQ'S</h5>
+                </a>
+
+            </div>
+
+            <div class="col-sm-4">
+                <span class="glyphicon glyphicon-certificate"></span>
+                <h4>Created By:</h4>
+                <br>
+                <p>By Adib,Tahia,Ridoy,Pronoy</p>
+            </div>
+            <div class="col-sm-4">
+                <span class="glyphicon glyphicon-wrench"></span>
+                <h4>Delivery </h4>
+                <br>
+                <p>We Delievr Our Product In Your DoorStep</p>
+
+            </div>
+            <br>
+        </div>
+        <br>
+        <h4> Our Payement Partner</h4>
+        <br>
+        <img src="images/rocket.png" width="70px" style="margin-right :20px;">
+        <img src="images/Nagad.png" width="70px" style="margin-right :20px;">
+        <img src="images/bkash.png" width="40px" style="margin-right :20px;">
+        <br><br>
+    </div>
+</strong>
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3 " style="background-color:##ffd966;">© 2023-December Copyright:
+    <a href="" target="_blank" style="color: black; text-decoration: none;">Mystic Trades</a>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
