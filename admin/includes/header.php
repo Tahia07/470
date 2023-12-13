5446a01cba664a5df84c9b336116acd13896bd12
Moinul Hossain <adibhossain0077@gmail.com>
2023-12-14 00:33:31 +0600

    Pushing

---
+++ admin/includes/header.php
@@ -0,0 +1,41 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #000000;">
+        <div class="container-fluid">
+            <a href="index.php"> <img src="../images/logo.png" id="image"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" style="color: brown; " aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+    <div class="navbar-nav">
+
+    
+
+    </div>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link " aria-current="page" href="index.php" style="color: white; font-size:bold;">DashBoard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="orders.php" style="color: white; font-size:bold;">Orders</a>
+                    </li>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="products.php" style="color: white; font-size:bold;">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="customers.php" style="color: white; font-size:bold;">Customers</a>
+                    </li>
+			         <li class="nav-item">
+				          <a class="nav-link" href="admin.php" style="color: white; font-size:bold;">Admin Info's</a>
+			         </li>
+			         <li class="nav-item">
+				          <a class="nav-link" href="../index.php" style="color: white; font-size:bold;">Go To Store's</a>
+			         </li>
+			         <li class="nav-item">
+				          <a class="nav-link" href="logout.php" style="color: red; font-size:bold;">SIGN OUT's</a>
+			         </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
